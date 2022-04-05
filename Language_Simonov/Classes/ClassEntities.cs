a60446f1f9e387e4cb5fda43a513206a3bb30991
Nikita Simonov <Nikitasimonov345633@hotmail.com>
2022-04-05 14:09:09 +0300

    Добавьте файлы проекта.

---
+++ Language_Simonov/Classes/ClassEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Language_Simonov.EF;
+
+namespace Language_Simonov.Classes
+{
+    internal class ClassEntities
+    {
+        public static Language context { get; set; } = new Language();
+    }
+}
